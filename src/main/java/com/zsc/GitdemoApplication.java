0a1a199eb2f50b77401341e2a7c5cb6ccf7403e4
zsc <zsc821123@163.com>
2019-12-18 13:27:30 +0800

    sout

--- src/main/java/com/zsc/GitdemoApplication.java
+++ src/main/java/com/zsc/GitdemoApplication.java
@@ -8,6 +8,7 @@ public class GitdemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GitdemoApplication.class, args);
+        System.out.println("第一个版本的修改");
     }
 
 }
