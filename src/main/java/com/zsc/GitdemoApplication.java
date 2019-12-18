440d18dd3ede4720ff97526c6d59c4ca2a1872e1
zsc <zsc821123@163.com>
2019-12-18 13:44:37 +0800

    add ("修bug")

--- src/main/java/com/zsc/GitdemoApplication.java
+++ src/main/java/com/zsc/GitdemoApplication.java
@@ -8,8 +8,17 @@ public class GitdemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GitdemoApplication.class, args);
+
         System.out.println("第一个版本的修改");
         System.out.println("Dev");
+
+
+        System.out.println("dasda");
+        System.out.println("dev");
+
+
+        System.out.println("");
+
     }
 
 }
